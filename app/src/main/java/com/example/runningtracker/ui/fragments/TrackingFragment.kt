1ec508b8efe1adf53868fc56e19fd587565eb374
hwa <HyunhwaShin@gmail.com>
2021-06-16 23:55:24 +0900

    map에 running track 그리기

--- app/src/main/java/com/example/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/TrackingFragment.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.runningtracker.R
+import com.example.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.runningtracker.other.Constants.MAP_ZOOM
+import com.example.runningtracker.other.Constants.POLYLINE_COLOR
+import com.example.runningtracker.other.Constants.POLYLINE_WIDTH
+import com.example.runningtracker.other.TrackingUtility
+import com.example.runningtracker.services.Polyline
 import com.example.runningtracker.services.TrackingService
 import com.example.runningtracker.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -18,6 +27,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel : MainViewModel by viewModels()
 
+    private var isTracking= false
+    private var pathPoints = mutableListOf<Polyline>()
+
     private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,13 +37,88 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
         mapView.onCreate(savedInstanceState)
         btnToggleRun.setOnClickListener {
-            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+           // sendCommandToService(ACTION_START_OR_RESUME_SERVICE) 아래걸로 바뀜
+            toggleRun()
         }
 
         mapView.getMapAsync {
             map = it
+            addAllPolylines()
+        }
+        subscribeToObservers()
+    }
+    private fun subscribeToObservers() {
+        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking(it)
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints = it
+            addLatestPolyline()
+            moveCameraToUser()
+        })
+    }
+
+    private fun toggleRun() {
+        if(isTracking) {
+           // menu?.getItem(0)?.isVisible = true
+            sendCommandToService(ACTION_PAUSE_SERVICE)
+        } else {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
     }
+
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if(!isTracking) {
+            btnToggleRun.text = "Start"
+            btnFinishRun.visibility = View.VISIBLE
+        } else {
+            btnToggleRun.text = "Stop"
+            //menu?.getItem(0)?.isVisible = true
+            btnFinishRun.visibility = View.GONE
+        }
+    }
+
+    //언제든 카메라를 사용자 위치로 이동시키는 함수
+    private fun moveCameraToUser() {
+        //적어도 내부에 하나의 좌표가 있는지 확인하는 작업
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    //pathPoints 는 우리가 얻은 가장 최신의 좌표이므로
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+    //장치 회전을 위해??
+    private fun addAllPolylines() {
+        for(polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyline(){
+        if(pathPoints.isEmpty() && pathPoints.last().size > 1){
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size -2]
+            val lastLatLng = pathPoints.last().last()
+
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
+        }
+
+    }
+
     private fun sendCommandToService(action: String) =
         Intent(requireContext(), TrackingService::class.java).also {
             it.action = action
